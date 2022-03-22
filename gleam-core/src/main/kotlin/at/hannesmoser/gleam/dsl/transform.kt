5362fbe5ed7be4f885e75d258d9d38799402d9a4
Hannes Moser <box@hannesmoser.at>
2022-03-22 15:27:11 +0100

    refactor: stay within the 80 character limit for a single line

--- gleam-core/src/main/kotlin/at/hannesmoser/gleam/dsl/transform.kt
+++ gleam-core/src/main/kotlin/at/hannesmoser/gleam/dsl/transform.kt
@@ -4,6 +4,7 @@ import org.apache.beam.sdk.transforms.PTransform
 import org.apache.beam.sdk.values.PCollection
 import org.apache.beam.sdk.values.POutput
 
-fun <T> transform(apply: (input: PCollection<T>) -> POutput) = object : PTransform<PCollection<T>, POutput>() {
-  override fun expand(input: PCollection<T>) = apply(input)
-}
+fun <T> transform(apply: (input: PCollection<T>) -> POutput) =
+  object : PTransform<PCollection<T>, POutput>() {
+    override fun expand(input: PCollection<T>) = apply(input)
+  }
